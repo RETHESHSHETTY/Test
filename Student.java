caa92eb01cbfd72680ed09ab2906703bc2ed3e1f
RETHESHSHETTY <retheshahv@gmail.com>
2024-01-11 12:44:33 +0530

    committed by dev1

--- Student.java
+++ Student.java
@@ -6,11 +6,28 @@ public int sub(int a,int b);
 
 
 }
-public class Studentimp{
+public class Studentimp implements Student{
     public int add(int a,int b){
         return a+b;
     }
     public int sub(int a,int b){
         return a-b;
     }
+}
+public class StudentMain extends Studentimp{
+    public static void main(String[] args){
+
+Scanner sc=new Scanner(System.in);
+System.out.println("enter 1st num");
+int a=sc.nextInt();
+System.out.println("enter 2nd num");
+int b=sc.nextInt();
+
+add(a,b);
+sub(a,b);
+
+    }
+
+    
+
 }
